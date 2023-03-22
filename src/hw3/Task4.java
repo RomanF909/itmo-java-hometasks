4680453b66b343d6dcab35ae7cebb0f365c1161e
RomanF <roman-f@mail.ru>
2023-03-22 15:48:40 +0300

    сообщение

--- src/hw3/Task4.java
+++ src/hw3/Task4.java
@@ -5,7 +5,7 @@ public class Task4 {
 
         Scanner taskScanner = new Scanner(System.in);
         System.out.println("Придумайте число от 2 до 100");
-        int a = (int) (4 + Math.random() * 101), b = 100, c = 2;
+        int a = (int) (4 + Math.random() * 101), b = 101, c = 2;
         System.out.println("Загаданное число равно " + a + "?");
         System.out.println("Введите ответ, 0 - НЕТ, 1 - ДА");
         int answer1 = taskScanner.nextInt();
@@ -15,11 +15,11 @@ public class Task4 {
             System.out.println("Введите ответ, 0 - НЕТ, 1 - ДА");
             int answer2 = taskScanner.nextInt();
           if (answer2 == 1) {c = a;
-              a = (int) a + (b - a) / 2;
+              a = a + (b - a) / 2;
               System.out.println("Загаданное число равно " + a + "?");
           }
           else {b = a;
-              a = (int) a - (a - c) / 2;
+              a = a - (a - c) / 2;
               System.out.println("Загаданное число равно " + a + "?");}
 
             System.out.println("Введите ответ, 0 - НЕТ, 1 - ДА");
